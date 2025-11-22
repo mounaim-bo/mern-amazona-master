185a5d0bcf3dd6860246951b35ddb12772c88f2e
Bouchra Hsine <hsinebouchra3@gmail.com>
2025-11-22 23:00:41 +0100

    MOD env.production and index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
+import axios from 'axios';   // ✅ ajouté
+
+axios.defaults.baseURL =
+  process.env.REACT_APP_API_URL ||
+  'https://amazona-backend-production.up.railway.app';  // ✅ backend Railway
 
 const root = createRoot(document.getElementById('root'));
 
@@ -22,7 +27,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
